09adbddaa27e306d06a8ce9d6a66a5e6b44c169e
yuyongwu <603938723@qq.com>
2019-11-21 23:38:59 +0800

    update blog

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('',views.index),
+    url(r'^$',views.index),
 ]
